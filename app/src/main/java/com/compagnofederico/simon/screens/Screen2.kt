3f2501e73a020c352198c92c5f42ca74c1946520
fedecompa04 <fede.compa04@gmail.com>
2026-04-04 13:21:35 +0200

    Main: did Screen2

--- app/src/main/java/com/compagnofederico/simon/screens/Screen2.kt
+++ app/src/main/java/com/compagnofederico/simon/screens/Screen2.kt
@@ -2,10 +2,22 @@ package com.compagnofederico.simon.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,11 +27,41 @@ fun Screen2(history: List<String>){
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ){
-
+        items(history){
+            game -> MatchResult(game)
+        }
     }
 }
 
-private fun countChar(s: String): Int{
+@Composable
+private fun MatchResult(game: String){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = Color(0xFFEEEEEE),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(32.dp)
+        ) {
+            Text(
+                text = CountChar(game).toString(),
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = game,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+private fun CountChar(s: String): Int{
     var count: Int = 0
     for(c in s){
         if(c != ',' && c != ' ') count++
