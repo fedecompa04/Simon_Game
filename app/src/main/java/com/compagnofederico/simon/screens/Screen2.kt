965bcce8796e1e2d4ce7bb7343ac59e14946d4c3
fedecompa04 <fede.compa04@gmail.com>
2026-04-17 20:56:06 +0200

    Feat: added summary title

--- app/src/main/java/com/compagnofederico/simon/screens/Screen2.kt
+++ app/src/main/java/com/compagnofederico/simon/screens/Screen2.kt
@@ -3,19 +3,24 @@ package com.compagnofederico.simon.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -29,6 +34,14 @@ fun Screen2(history: List<String>, currentSequence: String){
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ){
+        item{
+            Text(
+                text = stringResource(R.string.summary),
+                fontFamily = FontFamily.Monospace,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.ExtraBold,
+            )
+        }
         items(history) {
             game -> MatchResult(game, inProgress = false)
         }
